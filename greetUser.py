c1766264e0c4e09e584c91066e596dcb0d8dd351
YesimOguz <yessoguzzz@gmail.com>
2021-05-31 23:10:59 +0300

    first commit

---
+++ greetUser.py
@@ -0,0 +1,8 @@
+#get user's name and save it in a variable 
+name=input('what is your name?: ')
+#print the name
+print(name)
+
+#automaticly gives a space 
+print('merhaba',name)
+print('merhaba '+name)
